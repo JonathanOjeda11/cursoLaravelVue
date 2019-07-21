81aaff3752d9d37188368fe4dc884987aba6abe1
Jonathan Ojeda <jonathan.ojeda@cceo.com.mx>
2019-07-21 16:51:33 -0500

    Se agrego el modelo y el controlador

---
+++ System/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+ 
+    public function index()
+    {
+        $categories = Category::all();
+        return $categories;
+    }
+
+    public function store(Request $request)
+    {
+        $category = new Category();
+        $category->name=$request->name;
+        $category->descripton=$request->description;
+        $category->status='1';
+        $category->save();
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        $category = Category::findOrFail($request->id);
+        $category->name=$request->name;
+        $category->descripton=$request->description;
+        $category->status='1';
+        $category->save();
+    }
+
+    public function desactivar(Request $request)
+    {
+        $category = Category::findOrFail($request->id);
+        $category->status='0';
+        $category->save();
+    }
+
+    public function activar(Request $request)
+    {  
+        $category = Category::findOrFail($request->id);
+        $category->status='1';
+        $category->save();
+    }
+}
+
