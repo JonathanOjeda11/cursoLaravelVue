357e1c2c259321ab040ec1173f35e23eaa45ab11
Jonathan Ojeda <jonathan.ojeda@cceo.com.mx>
2019-07-23 18:08:17 -0500

    se agregaron los metodos store,index y update al clientController y se agregaron sus rutas

--- System/app/Http/Controllers/ClientController.php
+++ System/app/Http/Controllers/ClientController.php
@@ -3,8 +3,62 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Person;
 
 class ClientController extends Controller
 {
-    
+    public function index(Request $request)
+    {
+        if(!$request->ajax()) return redirect('/');
+        
+        $search=$request->search;
+        $criteria=$request->criteria;
+
+        if($search=='')
+        {
+            $Persons=Person::orderBy('id','desc')->paginate(3);
+        }else
+        {
+            $Persons=Person::where($criteria, 'like', '%'. $search . '%')->orderBy('id','desc')->paginate(3);
+        }
+
+        return [
+            'pagination' => [
+                'total' => $Persons->total(),
+                'current_page' => $Persons->currentPage(),
+                'per_page' => $Persons->perPage(),
+                'last_page' => $Persons->lastPage(),
+                'from' => $Persons->firstItem(),
+                'to' => $Persons->lastItem()
+            ],
+            'categories' => $categories
+        ];
+
+
+    }
+
+    public function store(Request $request)
+    {
+        if(!$request->ajax()) return redirect('/');
+        $person = new Person();
+        $person->name = $request->name;
+        $person->document_type = $request->document_type;
+        $person->document_num = $request->document_num;
+        $person->address = $request->address;
+        $person->phone= $request->phone;
+        $person->save();
+    }
+
+    public function update(Request $request)
+    {
+        if(!$request->ajax()) return redirect('/');
+        $person = Person::findOrFail($request->id);
+        $person->name = $request->name;
+        $person->document_type = $request->document_type;
+        $person->document_num = $request->document_num;
+        $person->address = $request->address;
+        $person->phone= $request->phone;
+        $person->save();
+    }
+
 }
--- System/app/Person.php
+++ System/app/Person.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    protected $fillable = ['name','document_type','document_num','address','phone','mail'];
 }
