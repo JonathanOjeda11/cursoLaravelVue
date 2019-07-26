3e3d9d1f3f5ff5c1cbc8b6dc628d1568079945a0
Jonathan Ojeda <jonathan.ojeda@cceo.com.mx>
2019-07-26 14:26:35 -0500

    develop updated

---
+++ System/resources/assets/js/components/Rol.vue
@@ -0,0 +1,204 @@
+<template>
+            <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">Home</li>
+                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+            <div class="container-fluid">
+                <!-- Ejemplo de tabla Listado -->
+                <div class="card">
+                    <div class="card-header">
+                        <i class="fa fa-align-justify"></i> Roles
+                        
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                <div class="input-group">
+                                    <select class="form-control col-md-3" id="opcion" name="opcion">
+                                      <option value="nombre">Nombre</option>
+                                      <option value="descripcion">Descripción</option>
+                                    </select>
+                                    <input type="text" v-model="search" @keyup.enter="listRol(1,search,criteria)" name="texto" class="form-control" placeholder="Texto a buscar">
+                                    <button type="submit" @click="listRol(1,search,criteria)" class="btn btn-primary"><i class="fa fa-search"></i> Buscar</button>
+                                </div>
+                            </div>
+                        </div>
+                        <table class="table table-bordered table-striped table-sm">
+                            <thead>
+                                <tr>
+                                   
+                                    <th>Nombre</th>
+                                    <th>Descripción</th>
+                                    <th>Estado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="rol in arrayRol" :key="rol.id">
+                                    
+                                    <td v-text="rol.name"></td>
+                                    <td v-text="rol.description"></td>
+                                    <td>
+                                        <div v-if="rol.status">
+                                            <span class="badge badge-success">Activo</span>
+                                        </div>
+                                        <div v-else>
+                                            <span class="badge badge-danger">Inactivo</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                               
+                            </tbody>
+                        </table>
+                        <nav>
+                            <ul class="pagination">
+                                <li class="page-item" v-if="pagination.current_page>1">
+                                    <a class="page-link" href="#" @click.prevent="changePage(pagination.current_page-1,search,criteria)">Ant</a>
+                                </li>
+                                <li class="page-item active" v-for="page in pagesNumber" :key="page" :class="[page==isActived ?  'active': '']">
+                                    <a class="page-link" href="#" @click.prevent="changePage(page,search,criteria)" v-text="page"></a>
+                                </li>
+                                <li class="page-item" v-if="pagination.current_page < pagination.last_page">
+                                    <a class="page-link" href="#" @click.prevent="changePage(pagination.current_page+1,search,criteria)">Sig</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <!-- Fin ejemplo de tabla Listado -->
+            </div>
+           
+            
+        </main>
+</template>
+
+<script>
+    export default {
+         data()
+            {
+                return {
+                    rol_id:0,
+                    name:'',
+                    description:'',
+                    arrayRol:[],
+                    modal:0,
+                    titleModal:'',
+                    typeAction:0,
+                    errorCategory:0,
+                    errorShowMsjCategory:[],
+                    pagination: {
+                        'total':0,
+                        'current_page':0, 
+                        'per_page':0,
+                        'last_page':0,
+                        'from' :0,
+                        'to':0,
+                    },
+                    offset:3,
+                    criteria: 'name',
+                    search: ''
+                   
+                }
+            },
+            computed:
+            {
+                isActived: function()
+                {
+                    return this.pagination.current_page;
+                },
+                pagesNumber:  function()
+                {
+                    if(!this.pagination.to)
+                    {
+                        return [];
+                    }
+                    var from = this.pagination.current_page - this.offset;
+                    if(from < 1)
+                    {
+                        from = 1;
+                    }
+
+                    var to = from+(this.offset*2);
+                    if(to>=this.pagination.last_page)
+                    {
+                        to = this.pagination.last_page;
+                    }
+                    var pagesArray=[];
+                    while(from<=to)
+                    {
+                    pagesArray.push(from);
+                    from++;
+                    }
+                    return pagesArray;
+
+
+                }
+            },
+        methods: 
+        {
+            listRol(page, search, criteria)
+            {
+               let me = this;
+                    var url = '/rol?page='+page+'&search='+search+'&criteria='+criteria;
+                    axios.get(url).then(function (response) {
+                        var answer = response.data;
+                        me.arrayRol=answer.roles.data
+                        me.pagination=answer.pagination;
+                        
+
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    })
+                    .then(function () {
+                        // always executed
+                    });  
+
+            },
+            
+            changePage(page, search, criteria)
+            {
+                    let me = this;
+                    me.pagination.current_page=page;
+                    me.listRol(page, search, criteria);
+
+            },
+            
+        },
+                
+
+        mounted() {
+            this.listRol(1, this.search, this.criteria);
+        }
+    
+        }
+        </script>
+        <style>
+        .mostrar
+        {
+            display: list-item !important;
+            opacity: 1 !important;
+            position: absolute !important;
+            background-color: #3c29297a !important;
+        }
+        .modal-content
+        {
+            width: 100% !important;
+            position: absolute;
+        }
+        .div-error
+        {
+            display: flex;
+            justify-content: center;
+
+        }
+        .text-error
+        {
+            color: red !important;
+            font-weight: bold;
+        }
+
+
+        </style>
